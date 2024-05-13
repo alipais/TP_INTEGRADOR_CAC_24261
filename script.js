379c8c8b3f254bed2c97ac5b8e7a40ac2e455579
alipais <aliciadri2011@gmail.com>
2024-05-13 11:18:47 -0300

    aguegue archivo js

--- script.js
+++ script.js
@@ -1,53 +1,47 @@
 // Función para el menú desplegable
 
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById("menu-toggle");
-    const menuList = document.querySelector(".menu-cerrado");
-
-    menuToggle.addEventListener("click", function() {
-        menuList.classList.toggle("menu-abierto");
-    });
-
-    document.addEventListener("click", function(event) {
-        const target = event.target;
-        if (!menuList.contains(target) && !menuToggle.contains(target)) {
-            menuList.classList.remove("menu-abierto");
+    document.addEventListener("DOMContentLoaded", function() {
+        var menuToggle = document.getElementById("menu-toggle");
+        var menuList = document.querySelector(".menu-cerrado");
+    
+        if (menuToggle && menuList) {
+            menuToggle.addEventListener("click", function() {
+                menuList.classList.toggle("menu-abierto");
+            });
         }
+    
+        document.addEventListener("click", function(event) {
+            const target = event.target;
+            if (!menuList.contains(target) && !menuToggle.contains(target)) {
+                menuList.classList.remove("menu-abierto");
+            }
+        });
+    
+        window.addEventListener("resize", function() {
+            // Cerrar el menú desplegable al cambiar el tamaño de la ventana
+            menuList.classList.remove("menu-abierto");
+        });
     });
 
-    window.addEventListener("resize", function() {
-        // Cerrar el menú desplegable al cambiar el tamaño de la ventana
-        menuList.classList.remove("menu-abierto");
-    });
-});
+    
+   //slider
+    var sliderItems = document.querySelectorAll('.slider-item');
+    var currentIndex = 0;
+    var sliderInterval;
 
- 
+    function changeSlide() {
+        sliderItems[currentIndex].style.opacity = 0;
+        currentIndex = (currentIndex + 1) % sliderItems.length;
+        sliderItems[currentIndex].style.opacity = 1;
+    }
 
-// Slider de imágenes
-var sliderItems = document.querySelectorAll('.slider-item');
-var currentIndex = 0;
+    function irATienda() {
+        clearInterval(sliderInterval); // Detener el intervalo del slider
+        window.location.href = 'shop.html';
+    }
 
-function changeSlide() {
-    sliderItems[currentIndex].style.opacity = 0;
-    currentIndex = (currentIndex + 1) % sliderItems.length;
-    sliderItems[currentIndex].style.opacity = 1;
-}
-
-setInterval(changeSlide, 3000); // Cambia la imagen cada 3 segundos
-
+    sliderInterval = setInterval(changeSlide, 3000); // Iniciar el intervalo del slider
 
 
-//<!-- Incluir la cabecera -->  
-//<div id="header-placeholder"></div>
-//<script>
-// fetch('header.html')
- //   .then(response => response.text())
-   // .then(html => document.getElementById('header-placeholder').innerHTML = html);
-//</script>
-//<!-- Incluir pie -->
-//<div id="footer-placeholder"></div>
-//<script>
-//  fetch('footer.html')
-//    .then(response => response.text())
-  //  .then(html => document.getElementById('footer-placeholder').innerHTML = html);
-//</script>
+
+
